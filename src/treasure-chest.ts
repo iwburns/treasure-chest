29a4453a91a186c181c1444642a6e44cb0853829
Ian <iwburns8@gmail.com>
2019-08-18 18:36:48 -0500

    setup initial working implementation

--- src/treasure-chest.ts
+++ src/treasure-chest.ts
@@ -1,6 +1,56 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+import { Optional } from '@iwburns/tupperware';
 
+export interface Config<Input, Output> {
+  computeKey: (input: Input) => string;
+  computeValue: (input: Input) => Output;
+}
+
+export interface KeyValuePair<Output> {
+  key: string,
+  value: Output,
+}
+
+export class Cache<Input, Output> {
+
+  private data: Map<string, Output>;
+  private config: Config<Input, Output>;
+
+  /**
+   * @hidden
+   */
+  constructor(config: Config<Input, Output>) {
+    this.data = new Map();
+    this.config = config;
+  }
+
+  private getCached(key: string): Optional<Output> {
+    return Optional.fromNullable(this.data.get(key));
+  }
+
+  private getComputed(input: Input, key: string): Output {
+    const value = this.config.computeValue(input);
+    this.data.set(key, value);
+    return value;
+  }
+
+  get(input: Input): Output {
+    const key = this.config.computeKey(input);
+    return this.getCached(key).unwrapOr(() => {
+      return this.getComputed(input, key);
+    });
+  }
+
+  set(key: string, value: Output): void {
+    this.data.set(key, value);
+  }
+
+  setMany(keyValuePairs: Array<KeyValuePair<Output>>) {
+    keyValuePairs.forEach(({ key, value }) => {
+      this.set(key, value);
+    })
+  }
+}
+
+export function createCache<Input, Output>(config: Config<Input, Output>) {
+  return new Cache(config);
 }
--- test/treasure-chest.test.ts
+++ test/treasure-chest.test.ts
@@ -1,14 +1,29 @@
-import DummyClass from "../src/treasure-chest"
+import { createCache } from "../src/treasure-chest"
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+describe("Basic Test", () => {
+  it("Should multiply numbers", () => {
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+    const cache = createCache({
+      computeKey(input: number): string {
+        return `${input}`;
+      },
+      computeValue(input: number): number {
+        return input * 10;
+      },
+    });
+
+    expect(cache.get(1)).toEqual(10);
+
+    cache.set('2', 12); // clearly wrong
+    expect(cache.get(2)).toEqual(12);
+
+    cache.setMany([
+      { key: '5', value: 50 },
+      { key: '6', value: 60 },
+      { key: '7', value: 70 },
+    ]);
+    expect(cache.get(5)).toEqual(50);
+    expect(cache.get(6)).toEqual(60);
+    expect(cache.get(7)).toEqual(70);
+  });
+});
